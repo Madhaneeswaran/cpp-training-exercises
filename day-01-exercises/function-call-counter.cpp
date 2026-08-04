5d5c84852835b99d1332ff757824b16ef4a2c5ee
Madhaneeswaran Pichumani <madhaneeswaran@solitontech.com>
2026-08-04 14:56:44 +0530

    Assignment 1 and Assignment 2
    Signed-off-by: Madhaneeswaran Pichumani <madhaneeswaran@solitontech.com>

---
+++ day-01-exercises/function-call-counter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void counter() {
+    static int count = 0;
+    ++count;
+
+    std::cout << "Function called :- " << count << std::endl;
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    counter();
+    
+    return 0;
+}
