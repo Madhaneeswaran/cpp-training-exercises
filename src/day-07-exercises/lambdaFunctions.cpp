28197551bc4167cab24dcfedcdad721a697b4f05
Madhaneeswaran Pichumani <madhaneeswaran@solitontech.com>
2026-08-20 18:33:53 +0530

    Address review comments
    Signed-off-by: Madhaneeswaran Pichumani <madhaneeswaran@solitontech.com>

--- src/day-07-exercises/lambdaFunctions.cpp
+++ src/day-07-exercises/lambdaFunctions.cpp
@@ -7,7 +7,7 @@ void compute(int a, int b, T func) {
 }
 
 int main() {
-    auto add = [](const int& a, const int& b) {
+    auto add = [](const int a, const int b) {
         return a + b;
     };
 
